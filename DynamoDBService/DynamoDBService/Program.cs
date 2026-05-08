25eea7e30196341460cb492cbea47a7f98ac338b
Abraham Sancho <josale998@gmail.com>
2026-05-08 01:01:08 -0600

    Added reset env var testing container compose

--- DynamoDBService/DynamoDBService/Program.cs
+++ DynamoDBService/DynamoDBService/Program.cs
@@ -4,6 +4,10 @@
     {
         bool resetMinistack = Environment.GetEnvironmentVariable("DYNAMODB_RESET") == "true";
 
+        Console.WriteLine(resetMinistack
+            ? "DYNAMODB_RESET is set to true. Ministack will be reset before running tests."
+            : "DYNAMODB_RESET is not set to true. Ministack will NOT be reset before running tests.");
+
         TestDynamoDBService testService = new();
 
         if(resetMinistack)
@@ -17,9 +21,10 @@
 
         await testService.RunTestsTableGSIOnDemand();
 
+
+
         // Plan for more comprehensive testing
         Console.WriteLine("\nPerformance Testing Plan:");
-        Console.WriteLine("1. Add pagination cases.");
         Console.WriteLine("2. Run multiple iterations (e.g., 10 runs at the same time) and calculate average, min, max times.");
         Console.WriteLine("3. Test with different RCUs (Read Capacity Units) to simulate throttling.");
         Console.WriteLine("4. Use Custom scripts for load testing IE docker compose services.");
@@ -28,7 +33,6 @@
         Console.WriteLine("7. Test with filters, projections to see impact.");
         Console.WriteLine("8. Add reset call of ministack to clear data between tests for consistency.");
         // adding size in KB being tested for more comprehensive performance testing.
-        // adding different size on testing item
         // using IDynamoDBContext
         // hot partition key vs???? cold partition key testing hash and range
 
